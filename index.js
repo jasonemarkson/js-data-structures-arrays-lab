db5e2323a8cde2c9e5b024a031b861464c21da33
jasonemarkson <jasonmarkson76@gmail.com>
2021-02-21 12:05:53 -0500

    commit tree 1e4e9645d4aea869444339cfbc6b08e29fd559b6

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+let drivers = []
+drivers.push("Milo", "Otis", "Garfield")
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name)
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift()
+}
+
+function appendDriver(name) {
+    const new_drivers = [...drivers, name]
+    return new_drivers
+}
+
+function prependDriver(name) {
+    const first_drivers = [name, ...drivers]
+    return first_drivers
+}
+
+function removeLastDriver() {
+    let remove_last = drivers.slice(0, drivers.length - 1)
+    return remove_last
+}
+
+function removeFirstDriver() {
+    let remove_first = drivers.slice(1)
+    return remove_first
+}
